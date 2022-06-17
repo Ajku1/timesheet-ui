57a4b11412c94f17a2bcc390664265fab5b4cb5e
Nikola Kostadinov <nikola_sk@abv.bg>
2022-06-17 13:20:49 +0300

    Implement registration

--- src/app/register/models/register-form-control-name-enum.ts
+++ src/app/register/models/register-form-control-name-enum.ts
@@ -1,4 +1,7 @@
 export enum RegisterFormControlName {
+  Name = 'Name',
+  ManagerId = 'ManagerId',
+  Email = 'Email',
   Username = 'Username',
   Password = 'Password'
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,26 +1,44 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {take} from 'rxjs/operators';
 import {environment} from '../../environments/environment';
 import {RegisterFormControlName} from './models/register-form-control-name-enum';
+import {Router} from '@angular/router';
+import {Route} from '../route.enum';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  readonly registerFormControlName: typeof RegisterFormControlName = RegisterFormControlName;
   error?: string;
   formGroup: FormGroup;
+  users: { name: string, id: string }[] = [];
 
-  constructor(private readonly httpClient: HttpClient) {
+  constructor(private readonly httpClient: HttpClient,
+              private readonly router: Router) {
     this.formGroup = new FormGroup({
-      [RegisterFormControlName.Username]:
+      [RegisterFormControlName.Name]:
         new FormControl<string | null>(null, [Validators.required, Validators.minLength(8)]),
+      [RegisterFormControlName.ManagerId]: new FormControl<string | null>(null, Validators.required),
+      [RegisterFormControlName.Email]: new FormControl<string | null>(null, Validators.required),
+      [RegisterFormControlName.Username]: new FormControl<string | null>(null, Validators.required),
       [RegisterFormControlName.Password]:
         new FormControl<string | null>(null, [Validators.required, Validators.minLength(8)])
     });
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get<{ name: string, id: string }[]>(`${environment.backendUrl}/users`)
+      .pipe(take(1))
+      .subscribe({
+        next: (users: { name: string, id: string }[]) => {
+          this.users = users;
+        }
+      });
   }
 
   register(): void {
@@ -30,7 +48,16 @@ export class RegisterComponent {
     }
     this.httpClient.post(`${environment.backendUrl}/register`, this.formGroup.value)
       .pipe(take(1))
-      .subscribe();
+      .subscribe({
+        next: () => {
+          this.router.navigate([Route.TimeEntries]);
+        },
+        error: (error: HttpErrorResponse) => {
+          if (error.status === 409) {
+            this.error = 'Email is already in use. Try another one.';
+          }
+        }
+      });
 
   }
 }
